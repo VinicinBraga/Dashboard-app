68ba37b5b628fafeb0339f8182e8ed1efc1c7313
VinicinBraga <vinicius@bragamatta.com.br>
2023-06-01 11:53:53 -0300

    Theme anda Rotes (Base Structures)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,28 @@
+import { Box, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []);
   return (
     <div className="app">
-      
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Routes>
+              <Route path="/" element={<div>dashboard page</div>} />
+              <Route
+                path="/predictions"
+                element={<div>predictions page</div>}
+              />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
