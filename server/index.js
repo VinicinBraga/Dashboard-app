3d3841455fab797c22716ecfe1b05baf9d784963
VinicinBraga <vinicius@bragamatta.com.br>
2023-06-02 15:44:17 -0300

    Server update

--- server/index.js
+++ server/index.js
@@ -5,9 +5,13 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
-import KPI from "./models/KPI.js";
 import kpiRoutes from "./routes/kpi.js";
-import { kpis } from "./data/data.js";
+import productRoutes from "./routes/product.js";
+import transactionRoutes from "./routes/transaction.js";
+import KPI from "./models/KPI.js";
+import Product from "./models/Product.js";
+import Transaction from "./models/Transaction.js";
+import { kpis, products, transactions } from "./data/data.js";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -22,7 +26,10 @@ app.use(cors());
 
 /* ROUTES */
 app.use("/kpi", kpiRoutes);
+app.use("/product", productRoutes);
+app.use("/transaction", transactionRoutes);
 
+/* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -35,7 +42,7 @@ mongoose
     /* ADD DATA ONE TIME ONLY OR AS NEEDED */
     await mongoose.connection.db.dropDatabase();
     KPI.insertMany(kpis);
-    // Product.insertMany(products);
-    // Transaction.insertMany(transactions);
+    Product.insertMany(products);
+    Transaction.insertMany(transactions);
   })
   .catch((error) => console.log(`${error} did not connect`));
