2a116cb68949c005ab6bfb7bd3d7869c2569a411
VinicinBraga <vinicius@bragamatta.com.br>
2023-06-02 15:05:33 -0300

    Rote update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { themeSettings } from "./theme";
 import Navbar from "./pages/navbar";
 import Dashboard from "./pages/dashboard";
+import Predictions from "./pages/predictions";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -18,6 +19,7 @@ function App() {
             <Navbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
+              <Route path="/predictions" element={<Predictions />} />
             </Routes>
           </Box>
         </ThemeProvider>
